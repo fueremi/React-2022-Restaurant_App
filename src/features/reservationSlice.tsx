2f59d4b4f4badfd7fadadc65c088f014ad0eae60
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-30 17:02:40 +0700

    craate store and slice

---
+++ src/features/reservationSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IReservation {
+  value: string[];
+}
+
+const initialState: IReservation = {
+  value: ["Selena"],
+};
+
+export const reservationSlice = createSlice({
+  name: "reservations",
+  initialState,
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+    removeReservation: (state, action: PayloadAction<number>) => {
+      state.value.splice(action.payload, 1);
+    },
+  },
+});
+
+export const { addReservation, removeReservation } = reservationSlice.actions;
+
+export default reservationSlice.reducer;
